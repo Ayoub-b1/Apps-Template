1f10eed3d518d7f7b0e573741b76472bd41c1944
unknown <ayoub.bourhfella1@gmail.com>
2024-10-29 13:47:51 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [],
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dyke": "#372E2E",
+        "gray": "#7B6C6B",
+        "black": "#08061B",
+        "secondary": "#B18D79",
+        "dark-purple": "#0F0B24",
+        "primary": "#FAECCF",
+        "dark-secondary": "#897459"
+      },
+      fontFamily: {
+        pthin: ["Poppins-Thin", "sans-serif"],
+        pextralight: ["Poppins-ExtraLight", "sans-serif"],
+        plight: ["Poppins-Light", "sans-serif"],
+        pregular: ["Poppins-Regular", "sans-serif"],
+        pmedium: ["Poppins-Medium", "sans-serif"],
+        psemibold: ["Poppins-SemiBold", "sans-serif"],
+        pbold: ["Poppins-Bold", "sans-serif"],
+        pextrabold: ["Poppins-ExtraBold", "sans-serif"],
+        pblack: ["Poppins-Black", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
